d6a23031ce642cd4ddd2462fd59191574765057d
phambt <brandon.pham22@gmail.com>
2024-10-21 16:10:26 -0400

    adding jest config file

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/**/*.ts", // Collect from all TypeScript files in src/
+        "!src/**/index.ts" // Optionally exclude certain files (like entry points)
+    ],
+    coverageDirectory: "coverage",
+    coverageReporters: ["text", "lcov"],
+    testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+    coveragePathIgnorePatterns: [
+      "/node_modules/",
+      "/dist/"
+    ],
+    transform: {
+      "^.+\\.ts$": "ts-jest"
+    },
+    testTimeout: 30000  // Global test timeout of 30 seconds
+  };
+  
